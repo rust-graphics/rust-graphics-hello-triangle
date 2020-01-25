b169ef6b2cfe2beaff989f7c5f78bd66d52ba6c2
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-01-25 23:54:47 +0330

    a helper script for android build added and better project structure and getting ready for window library for android

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-extern crate rust_graphics_log as log;
-use log::log_i;
+mod main;
 
 #[allow(dead_code)]
 #[no_mangle]
 pub unsafe extern "C" fn rust_graphics_main(_: *mut u32, _: *mut u32, _: isize) {
-    log_i!("Hello from rust");
+    main::main();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+extern crate rust_graphics_log as log;
+
+use self::log::log_i;
+
+pub fn main() {
+    log_i!("Hello from rust");
+}
