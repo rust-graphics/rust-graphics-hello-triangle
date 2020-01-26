43233ec89120397d3225c945ea1179142dea2536
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-01-26 17:00:00 +0330

    main crate added

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod main;
-
-#[allow(dead_code)]
-#[no_mangle]
-pub unsafe extern "C" fn rust_graphics_main(_: *mut u32, _: *mut u32, _: isize) {
-    main::main();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
 extern crate rust_graphics_log as log;
+extern crate rust_graphics_main as main;
+// extern crate rust_graphics_window as window;
 
-use self::log::log_i;
+use {
+    self::log::{log_i, result_f},
+    main::main,
+    std::sync::{Arc, RwLock},
+};
 
-pub fn main() {
-    log_i!("Hello from rust");
-}
+// struct Listener {
+//     pub running: bool,
+// }
+
+// impl window::event::Listener for Listener {
+//     fn on_event(&mut self, event: &window::event::Event) -> bool {
+//         match event.get_data() {
+//             &window::event::Data::Quit => self.running = false,
+//             _e @ _ => {
+//                 #[cfg(feature = "debug_derive")]
+//                 log_i!("{:?}", _e);
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// pub fn main() {
+//     let w = window::Window::new();
+//     let listener = Arc::new(RwLock::new(Listener { running: true }));
+//     let l: Arc<RwLock<dyn window::event::Listener>> = listener.clone();
+//     w.get_engine().add(0, Arc::downgrade(&l));
+//     while { result_f!(listener.read()).running } {
+//         w.fetch_events();
+//     }
+//     log_i!("Program ended.");
+// }
+
+main! {{
+    log_i!("Program ended.");
+}}
