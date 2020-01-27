94930a88394b9f0caa1dc197857fc3620d554363
Hossein Noroozpour <hossein.noroozpour@gmail.com>
2020-01-27 18:32:27 +0330

    it's now can work on android

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,38 @@ extern crate rust_graphics_main as main;
 extern crate rust_graphics_window as window;
 
 use {
-    self::log::log_i, // result_f},
-    // std::sync::{Arc, RwLock},
-    self::window::create_window,
-    main::main,
+    self::log::{log_i, result_f},
+    self::window::Window,
+    main::{main, Arg},
+    std::sync::{Arc, RwLock},
 };
 
-// struct Listener {
-//     pub running: bool,
-// }
+struct Listener {
+    pub running: bool,
+}
 
-// impl window::event::Listener for Listener {
-//     fn on_event(&mut self, event: &window::event::Event) -> bool {
-//         match event.get_data() {
-//             &window::event::Data::Quit => self.running = false,
-//             _e @ _ => {
-//                 #[cfg(feature = "debug_derive")]
-//                 log_i!("{:?}", _e);
-//             }
-//         }
-//         return false;
-//     }
-// }
+impl window::event::Listener for Listener {
+    fn on_event(&mut self, event: &window::event::Event) -> bool {
+        match event.get_data() {
+            &window::event::Data::Quit => self.running = false,
+            _e @ _ => {
+                #[cfg(feature = "debug_derive")]
+                log_i!("{:?}", _e);
+            }
+        }
+        return false;
+    }
+}
 
-// pub fn main() {
-//     let w = window::Window::new();
-//     let listener = Arc::new(RwLock::new(Listener { running: true }));
-//     let l: Arc<RwLock<dyn window::event::Listener>> = listener.clone();
-//     w.get_engine().add(0, Arc::downgrade(&l));
-//     while { result_f!(listener.read()).running } {
-//         w.fetch_events();
-//     }
-//     log_i!("Program ended.");
-// }
-
-main! {{
-    let mut win = create_window!();
+fn start(arg: Arg) {
+    let w = Window::new(arg);
+    let listener = Arc::new(RwLock::new(Listener { running: true }));
+    let l: Arc<RwLock<dyn window::event::Listener>> = listener.clone();
+    w.get_event_engine().add(0, Arc::downgrade(&l));
+    while { result_f!(listener.read()).running } {
+        w.fetch_events();
+    }
     log_i!("Program ended.");
-}}
+}
+
+main!(start);
